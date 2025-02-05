dc0d5d9f6bb2c31ad01da0e4a431f525fd530420
yayamv <kendravedu@gmail.com>
2025-02-05 13:38:04 -0500

    Add files via upload

---
+++ stage-zero-task.py
@@ -0,0 +1,56 @@
+'''
+Kendra Vilfort
+1/5/2025
+HackBio Biocoding Internship
+Stage Zero Task
+'''
+
+name= ["Kendra Vilfort", "Lateefat Shuaib", "Pelumi  Okunrounmu", "BamideleBamidele" ]
+slack_username= ["Kendra", "Lateefat","Akinsete", "Dami O"]
+email= ["kendravedu@gmail.com", "ayomidelateefat23@gmail.com", "pelumiokuns@gmail.com", "bamideleiyoha@gmail.com"]
+hobby= ["art", "Volunteering", "poetry", "art"]
+country= ["USA", "Nigeria", "Nigeria", "UK"]
+discipline= ["Molecular Biology", "Plant Pathology", "Molecular Biology", "Biomedical Science"]
+prefered_programming_language= ["Python", "Python", "Python", "Python"]
+
+print("Name:", name[0])
+print("Slack Username:", slack_username[0])
+print("Email:", email[0])
+print("Hobby:", hobby[0])
+print("Country:", country[0])
+print("Discipline:", discipline[0])
+print("Preferred Programming Language:", prefered_programming_language[0])
+print("")
+print("-----------------------------------------")
+print("")
+
+print("Name:", name[1])
+print("Slack Username:", slack_username[1])
+print("Email:", email[1])
+print("Hobby:", hobby[1])
+print("Country:", country[1])
+print("Discipline:", discipline[1])
+print("Preferred Programming Language:", prefered_programming_language[1])
+print("")
+print("-----------------------------------------")
+print("")
+
+print("Name:", name[2])
+print("Slack Username:", slack_username[2])
+print("Email:", email[2])
+print("Hobby:", hobby[2])
+print("Country:", country[2])
+print("Discipline:", discipline[2])
+print("Preferred Programming Language:", prefered_programming_language[2])
+print("")
+print("-----------------------------------------")
+print("")
+
+print("Name:", name[3])
+print("Slack Username:", slack_username[3])
+print("Email:", email[3])
+print("Hobby:", hobby[3])
+print("Country:", country[3])
+print("Discipline:", discipline[3])
+print("Preferred Programming Language:", prefered_programming_language[3])
+
